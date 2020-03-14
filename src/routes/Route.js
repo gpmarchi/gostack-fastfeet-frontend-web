33579825d27b52f62e0fece1109790f211747c7f
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-14 18:24:18 -0300

    custom route component for private routing

---
+++ src/routes/Route.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = true;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOf([PropTypes.element, PropTypes.func]).isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import Route from './Route';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import Deliveryman from '../pages/Deliveryman';
@@ -12,10 +13,10 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/deliveryman" component={Deliveryman} />
-      <Route path="/parcel" component={Parcel} />
-      <Route path="/recipient" component={Recipient} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/deliveryman" component={Deliveryman} isPrivate />
+      <Route path="/parcel" component={Parcel} isPrivate />
+      <Route path="/recipient" component={Recipient} isPrivate />
     </Switch>
   );
 }
