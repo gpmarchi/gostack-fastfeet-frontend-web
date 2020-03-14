68abfb6078ebe042669865229b62ccc3040504b9
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-14 18:48:45 -0300

    dashboard common layout

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,13 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import DashboardLayout from '../pages/_layouts/dashboard';
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = true;
+  const signed = false;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -17,7 +20,21 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
+  if (signed) {
+    const Layout = DashboardLayout;
+
+    return (
+      <Route
+        {...rest}
+        render={props => (
+          <Layout>
+            <Component {...props} />
+          </Layout>
+        )}
+      />
+    );
+  }
+
   return <Route {...rest} component={Component} />;
 }
 
