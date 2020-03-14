a397d281641a76440260661cce22eea576e5c0ba
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-14 18:02:30 -0300

    basic pages structure

---
+++ src/pages/Parcel/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Parcel() {
+  return (
+    <div>
+      <h1>Parcel</h1>
+    </div>
+  );
+}
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1>Sign in</h1>
+    </div>
+  );
+}
