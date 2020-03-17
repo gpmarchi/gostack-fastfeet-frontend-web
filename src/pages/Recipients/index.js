41ac33a33f993a88b3512df6fe7091c4f8cae5b3
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-17 11:49:43 -0300

    recipients list styling

--- src/pages/Recipients/index.js
+++ src/pages/Recipients/index.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MdAdd, MdMoreHoriz } from 'react-icons/md';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
 export default function Recipients() {
+  const [recipients, setRecipients] = useState([]);
+
+  useEffect(() => {
+    async function loadRecipients() {
+      const response = await api.get('/recipients');
+
+      const data = response.data.map(recipient => ({
+        ...recipient,
+        address: `${recipient.street}, ${recipient.number}, ${recipient.city} - ${recipient.state}`,
+      }));
+
+      setRecipients(data);
+    }
+
+    loadRecipients();
+  }, []);
+
   return (
-    <div>
-      <h1>Recipients</h1>
-    </div>
+    <>
+      <header>
+        <h1>Gerenciando destinatários</h1>
+        <div>
+          <input type="text" placeholder="Buscar por destinatários" />
+          <button type="button">
+            <MdAdd size={23} /> CADASTRAR
+          </button>
+        </div>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Endereço</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recipients.map(recipient => (
+            <tr>
+              <td>#{recipient.id}</td>
+              <td>{recipient.name}</td>
+              <td>{recipient.address}</td>
+              <td>
+                <button type="button">
+                  <MdMoreHoriz size={25} color="#C6C6C6" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
