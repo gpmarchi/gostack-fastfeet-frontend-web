ac3213ba4ae84a246e21ed58292bea858b6b0be5
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-21 15:14:47 -0300

    return to intial page to parcels on user sign out

--- src/store/modules/header/reducer.js
+++ src/store/modules/header/reducer.js
@@ -6,6 +6,8 @@ export default function header(state = INITIAL_STATE, action) {
   switch (action.type) {
     case '@header/CHANGE_PAGE':
       return { activePage: action.payload };
+    case '@auth/SIGN_OUT':
+      return INITIAL_STATE;
     default:
       return state;
   }
